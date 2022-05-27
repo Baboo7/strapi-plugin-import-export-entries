f27b3c96769daa6b73ce0339017783e7335ad709
Baboo7 <dbsharp404@gmail.com>
2022-05-27 18:16:07 +0200

    fix: Update request urls

--- admin/src/api/exportProxy/ExportProxy.js
+++ admin/src/api/exportProxy/ExportProxy.js
@@ -1,7 +1,9 @@
 import { request } from "@strapi/helper-plugin";
 
+import pluginId from "../../pluginId";
+
 const getByContentType = async ({ slug, search, applySearch }) => {
-  const data = await request(`/import-export/export/contentTypes`, {
+  const data = await request(`/${pluginId}/export/contentTypes`, {
     method: "POST",
     body: { slug, search, applySearch },
   });
--- admin/src/api/importProxy/ImportProxy.js
+++ admin/src/api/importProxy/ImportProxy.js
@@ -1,7 +1,9 @@
 import { request } from "@strapi/helper-plugin";
 
+import pluginId from "../../pluginId";
+
 const importData = async ({ slug, data, format }) => {
-  const resData = await request(`/import-export/import`, {
+  const resData = await request(`/${pluginId}/import`, {
     method: "POST",
     body: { slug, data, format },
   });
