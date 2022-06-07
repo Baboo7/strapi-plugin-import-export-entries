252c09fb0054e4c0a2da455df0b83cae8ac07dba
Baboo7 <dbsharp404@gmail.com>
2022-06-07 23:16:41 +0200

    chore(Alerts): Clean imports

--- admin/src/components/Injected/Alerts/Alerts.js
+++ admin/src/components/Injected/Alerts/Alerts.js
@@ -1,13 +1,8 @@
 import { Alert } from "@strapi/design-system/Alert";
-import { Button } from "@strapi/design-system/Button";
 import { Portal } from "@strapi/design-system/Portal";
-import Download from "@strapi/icons/Download";
 import React from "react";
-import { useIntl } from "react-intl";
 
 import "./style.css";
-import getTrad from "../../../utils/getTrad";
-import { ExportModal } from "../../ExportModal";
 import { useAlerts } from "../../../hooks/useAlerts";
 
 export const Alerts = () => {
