c5c8727285f12f8b086300f1807038611ee53474
Baptiste Studer <dbsharp404@gmail.com>
2023-04-22 20:47:26 +0200

    fix(import-data): Check content permissions when import whole database

--- src/server/controllers/admin/import-controller/import-data.js
+++ src/server/controllers/admin/import-controller/import-data.js
@@ -1,8 +1,11 @@
-'use strict';
+import { CustomSlugs } from '../../../config/constants';
+import { getAllSlugs } from '../../../utils/models';
 
 const { getService } = require('../../../utils');
 
-const importData = async (ctx) => {
+module.exports = ({ strapi }) => importData;
+
+async function importData(ctx) {
   if (!hasPermissions(ctx)) {
     return ctx.forbidden();
   }
@@ -31,15 +34,24 @@ const importData = async (ctx) => {
   ctx.body = {
     failures: res.failures,
   };
-};
+}
 
-const hasPermissions = (ctx) => {
+function hasPermissions(ctx) {
   let { slug } = ctx.request.body;
   const { userAbility } = ctx.state;
 
+  let slugsToCheck = [];
+  if (slug === CustomSlugs.WHOLE_DB) {
+    slugsToCheck.push(...getAllSlugs());
+  } else {
+    slugsToCheck.push(slug);
+  }
+
+  return slugsToCheck.every((slug) => hasPermissionForSlug(userAbility, slug));
+}
+
+function hasPermissionForSlug(userAbility, slug) {
   const permissionChecker = strapi.plugin('content-manager').service('permission-checker').create({ userAbility, model: slug });
 
   return permissionChecker.can.create() && permissionChecker.can.update();
-};
-
-module.exports = ({ strapi }) => importData;
+}
