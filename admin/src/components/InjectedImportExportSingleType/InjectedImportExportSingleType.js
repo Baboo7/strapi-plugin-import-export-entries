9fb740e6074cde771c9e7e9df282771fe1d42275
Baptiste Studer <dbsharp404@gmail.com>
2023-05-21 22:13:11 +0200

    chore(InjectedImportExportSingleType): Clean

--- admin/src/components/InjectedImportExportSingleType/InjectedImportExportSingleType.js
+++ admin/src/components/InjectedImportExportSingleType/InjectedImportExportSingleType.js
@@ -3,23 +3,13 @@ import { Divider } from '@strapi/design-system/Divider';
 import { Typography } from '@strapi/design-system/Typography';
 import React from 'react';
 import { useIntl } from 'react-intl';
-import { useLocation } from 'react-router-dom';
 
 import getTrad from '../../utils/getTrad';
 import { InjectedExportButton } from '../InjectedExportButton';
 import { InjectedImportButton } from '../InjectedImportButton';
 
 export const InjectedImportExportSingleType = () => {
-  const { pathname } = useLocation();
   const { formatMessage } = useIntl();
-
-  const isCollectionType = () => {
-    return pathname.split('/')?.[2] === 'collectionType';
-  };
-
-  if (isCollectionType()) {
-    return null;
-  }
 
   return (
     <Box background="neutral0" hasRadius shadow="filterShadow" paddingTop={6} paddingBottom={4} paddingLeft={3} paddingRight={3}>
@@ -33,7 +23,7 @@ export const InjectedImportExportSingleType = () => {
       <Box paddingBottom={1}></Box>
       <InjectedImportButton fullWidth />
       <Box paddingBottom={2}></Box>
-      <InjectedExportButton fullWidth />
+      <InjectedExportButton fullWidth unavailableOptions={['exportPluginsContentTypes']} />
     </Box>
   );
 };
